febde17da70a8cc5ec7f964bae91c76354a149b6
Sakthivel <54572214+sakthivel1880@users.noreply.github.com>
2023-07-06 16:01:04 +0530

    Add files via upload
    first commit

---
+++ formvalidate.js
@@ -0,0 +1,26 @@
+;<html>
+	<body>
+		<form>
+			<div>
+				<label>UserName</label>
+				<input type='text' id='username' class='username' />
+			</div>
+			<div>
+				<label>email</label>
+				<input type='text' id='username' class='username' />
+			</div>
+			<div>
+				<label>phoneNumber</label>
+				<input type='text' id='username' class='username' />
+			</div>
+			<div>
+				<label>age</label>
+				<input type='text' id='username' class='username' />
+			</div>
+			<div>
+				<input type='submit' name='submit' value='submit' />
+			</div>
+		</form>
+	</body>
+	<script></script>
+</html>
